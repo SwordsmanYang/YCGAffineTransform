dbe653a59a63ab908e8578aa703fcf3662ba83ce
yangchangqing <yangcq@yunovo.cn>
2016-07-11 17:57:37 +0800

    Initial Commit

---
+++ CGAffineTransform/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CGAffineTransform
+//
+//  Created by ChangQing Yang on 16/7/11.
+//  Copyright © 2016年 ChangQing Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CGAffineTransform/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CGAffineTransform
+//
+//  Created by ChangQing Yang on 16/7/11.
+//  Copyright © 2016年 ChangQing Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
